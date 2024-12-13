a92c1376f97f140c8efd1176e1ae61fd77cf3b31
Gabriel <gs303291@gmail.com>
2024-12-12 22:33:33 -0300

    criando a função de calculo do determinante

--- script/script.js
+++ script/script.js
@@ -1,20 +1,34 @@
-const n1 = document.querySelector("#n1")
-const n2 = document.querySelector("#n2")
-const n3 = document.querySelector("#n3")
-const n4 = document.querySelector("#n4")
-const n5 = document.querySelector("#n5")
-const n6 = document.querySelector("#n6")
-const n7 = document.querySelector("#n7")
-const n8 = document.querySelector("#n8")
-const n9 = document.querySelector("#n9")
-
 function calcular() {
-    let matriz =
-        [
-            [n1.value, n2.value, n3.value],
-            [n4.value, n5.value, n6.value],
-            [n7.value, n8.value, n9.value]
-        ]
+  // buscando as informações da matriz
+  const n1 = parseFloat(document.querySelector("#n1").value);
+  const n2 = parseFloat(document.querySelector("#n2").value);
+  const n3 = parseFloat(document.querySelector("#n3").value);
+  const n4 = parseFloat(document.querySelector("#n4").value);
+  const n5 = parseFloat(document.querySelector("#n5").value);
+  const n6 = parseFloat(document.querySelector("#n6").value);
+  const n7 = parseFloat(document.querySelector("#n7").value);
+  const n8 = parseFloat(document.querySelector("#n8").value);
+  const n9 = parseFloat(document.querySelector("#n9").value);
 
-    console.table(matriz)
+  let matriz = [
+    [n1, n2, n3],
+    [n4, n5, n6],
+    [n7, n8, n9],
+  ];
+
+//   mostra o resultado
+  document.querySelector("#det").innerHTML = determinante(matriz);
+}
+
+function determinante(m) {
+  // função calcula a matriz usando a regra de Sarrus
+  // a função só funciona para matrizes 3x3
+  return (
+    m[0][0] * m[1][1] * m[2][2] +
+    m[0][1] * m[1][2] * m[2][0] +
+    m[0][2] * m[1][0] * m[2][1] -
+    (m[0][2] * m[1][1] * m[2][0] +
+      m[0][0] * m[1][2] * m[2][1] +
+      m[0][1] * m[1][0] * m[2][2])
+  );
 }
